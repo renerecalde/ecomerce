bae7649f9896dc538d82062d636b6930a4dc44ac
rene <renerecalde2@gmail.com>
2020-01-11 10:07:13 -0300

    Antes del despliegue

--- config/bundles.php
+++ config/bundles.php
@@ -58,4 +58,7 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true, 'test_cached' => true],
     Sylius\Behat\Application\SyliusTestPlugin\SyliusTestPlugin::class => ['test' => true, 'test_cached' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    SitemapPlugin\SitemapPlugin::class => ['all' => true], // Sitemap support
+    BitBag\SyliusCmsPlugin\BitBagSyliusCmsPlugin::class  => ['all' => true],
 ];
